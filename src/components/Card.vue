1c309eda056613cb33f8fa86fcb5660f538462c7
mooenz <joss92821@hotmail.com>
2023-04-12 19:09:23 -0500

    init project

---
+++ src/components/Card.vue
@@ -0,0 +1,14 @@
+<template>
+  <article>
+    <section>
+      <header>ADVICE #<span></span></header>
+      <h1>""</h1>
+      <footer>
+      </footer>
+    </section>
+
+    <section>
+      <div></div>
+    </section>
+  </article>
+</template>
